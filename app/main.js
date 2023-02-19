2a491d21a36304daaf8ae567d7dc2033ae1110bd
Gabriel Berto <gabrielberto@protonmail.com>
2023-02-19 02:15:58 -0300

    feat: get page size

--- app/main.js
+++ app/main.js
@@ -16,8 +16,8 @@ if (command === ".dbinfo") {
   console.log("Logs from your program will appear here!");
 
   // Uncomment this to pass the first stage
-  // const pageSize = buffer.readUInt16BE(16); // page size is 2 bytes starting at offset 16
-  // console.log(`database page size: ${pageSize}`);
+  const pageSize = buffer.readUInt16BE(16); // page size is 2 bytes starting at offset 16
+  console.log(`database page size: ${pageSize}`);
 } else {
   throw `Unknown command ${command}`;
 }
