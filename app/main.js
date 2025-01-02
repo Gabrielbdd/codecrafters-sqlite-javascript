505d76f75956a366084021eb951c3cb3f0466a8b
Gabriel Berto <gabriel@bdd.is>
2025-01-02 01:30:54 -0300

    feat: implement .tables command

--- app/main.js
+++ app/main.js
@@ -27,6 +27,17 @@ async function main() {
 		if (page.header.type === 13) {
 			console.log(`number of tables: ${page.header.numberOfCells}`);
 		}
+	} else if (command === ".tables") {
+		const tables = db.page.cells
+			.flatMap((cell) => {
+				const tableName = cell.record.body.columns[2];
+				if (tableName.startsWith("sqlite_")) {
+					return [];
+				}
+				return [tableName];
+			})
+			.join(" ");
+		console.log(tables);
 	} else {
 		throw `Unknown command ${command}`;
 	}
@@ -39,7 +50,7 @@ class Database {
 	constructor(buffer) {
 		this.buffer = buffer;
 		this.header = new DatabaseHeader(this.buffer.subarray(0, 100));
-		this.page = new Page(buffer);
+		this.page = new Page(buffer, true);
 	}
 }
 
@@ -54,14 +65,19 @@ class DatabaseHeader {
 	get pageSize() {
 		return this.buffer.readUInt16BE(16); // page size is 2 bytes starting at offset 16
 	}
+
+	get encoding() {
+		return this.buffer.readUInt32BE(56);
+	}
 }
 
 class Page {
 	/**
 	 * @param {Buffer} buffer
 	 */
-	constructor(buffer) {
-		this.buffer = buffer.subarray(100); // assume this is the first page, so ignore the first 100 bytes as it is for the database header
+	constructor(buffer, isFirst = false) {
+		this.originalBuffer = buffer;
+		this.buffer = isFirst ? buffer.subarray(100) : buffer;
 		this.header = new PageHeader(this.buffer.subarray(0, 8)); // assuming this is a table page (it is 12 on index page)
 	}
 
@@ -79,7 +95,7 @@ class Page {
 	get cells() {
 		const result = [];
 		for (const pointer of this.cellPointerArray) {
-			result.push(this.buffer.readUint8(pointer - this.buffer.byteOffset)); // subtract buffer offset because the offset is meant to be referenced from the start of the file
+			result.push(new Cell(this.originalBuffer.subarray(pointer))); // the offsets are relative to the start of the page
 		}
 		return result;
 	}
@@ -109,4 +125,155 @@ class PageHeader {
 	get numberOfCells() {
 		return this.buffer.readUint16BE(3); // page cells count is the 2 bytes at offset 3
 	}
+}
+
+class Cell {
+	/**
+	 * @param {Buffer} buffer
+	 */
+	constructor(buffer) {
+		this.buffer = buffer;
+		this.reader = new BinaryReader(buffer);
+		this.recordSize = this.reader.readVarint();
+		this.rowid = this.reader.readVarint();
+		this.record = new Record(buffer.subarray(this.reader.pos));
+	}
+}
+
+class Record {
+	/**
+	 * @param {Buffer} buffer
+	 */
+	constructor(buffer) {
+		this.buffer = buffer;
+		this.reader = new BinaryReader(this.buffer);
+		this.header = new RecordHeader(this.reader);
+		this.body = new RecordBody(this.reader, this.header);
+	}
+}
+
+class RecordHeader {
+	/**
+	 * @param {BinaryReader} buffer
+	 */
+	constructor(reader) {
+		this.reader = reader;
+		this.size = this.reader.readVarint();
+		this.serialTypes = this.readSerialTypes();
+	}
+
+	readSerialTypes = () => {
+		const result = [];
+
+		while (this.reader.pos !== this.size) {
+			result.push(this.reader.readVarint());
+		}
+
+		return result;
+	};
+}
+
+class RecordBody {
+	/**
+	 * @param {BinaryReader} buffer
+	 */
+	constructor(reader, header) {
+		this.reader = reader;
+		this.columns = this.readColumns(header);
+	}
+
+	readColumns = (header) => {
+		const result = [];
+
+		for (const serialType of header.serialTypes) {
+			const size = getSerialTypeSize(serialType);
+
+			if (size === 1) {
+				result.push(this.reader.buffer.readUint8(this.reader.pos));
+				this.reader.pos += 1;
+			} else {
+				const bytes = this.reader.buffer.subarray(
+					this.reader.pos,
+					this.reader.pos + size,
+				);
+				const decoder = new TextDecoder();
+				result.push(decoder.decode(bytes));
+				this.reader.skip(size);
+			}
+		}
+
+		return result;
+	};
+}
+
+class BinaryReader {
+	/**
+	 * @param {Buffer} buffer
+	 */
+	constructor(buffer) {
+		this.buffer = buffer;
+		this.pos = 0;
+	}
+
+	/**
+	 * @param {number} n
+	 */
+	skip = (n) => {
+		this.pos += n;
+	};
+
+	/**
+	 * @param {number} n
+	 */
+	setPos = (n) => {
+		this.pos = n;
+	};
+
+	readVarint = () => {
+		let byte = this.buffer.readUint8(this.pos);
+		this.pos += 1;
+
+		let result = byte & 0x7f; // remove msb
+
+		// check if msb is NOT set
+		if (!((byte & 0x80) > 0)) {
+			// if not, then we are done, just return the result
+			return result;
+		}
+
+		// if it is set, then next byte must be read
+		byte = this.buffer.readUint8(this.pos);
+		this.pos += 1;
+
+		result = (result << 7) | (byte & 0x7f);
+
+		// TODO: handle more than two byte varint
+
+		return result;
+	};
+}
+
+function getSerialTypeSize(serialType) {
+	if (serialType <= 4) {
+		return serialType;
+	}
+
+	if (serialType === 5) {
+		return 6;
+	}
+
+	if (serialType === 6 || serialType === 7) {
+		return 8;
+	}
+
+	if (serialType === 8 || serialType === 9) {
+		return 0;
+	}
+
+	// if even
+	if (serialType % 2 === 0) {
+		return (serialType - 12) / 2;
+	}
+
+	return (serialType - 13) / 2;
 }
